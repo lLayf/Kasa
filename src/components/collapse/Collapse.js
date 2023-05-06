9432d24ee724005e17eb1b5acc57a8726fe2077b
Cédric <fourrecedric@gmail.com>
2023-05-06 15:10:20 +0200

    Page "home" réalisée composant collapse crée

---
+++ src/components/collapse/Collapse.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import './collapse.css';
+import arrowDown from '../../assets/img/arrow-down.svg';
+import arrowUp from '../../assets/img/arrow-up.svg';
+
+function Collapse(props) {
+  const [open, setOpen] = useState(true);
+
+  return (
+    <div className="collapse">
+      <button className='collapse-btn' onClick={() => setOpen(!open)}>
+        <h3>{props.title}</h3>
+        <img src={open ? arrowDown : arrowUp} alt="icon menu" />
+      </button>
+      <div className={open ? 'collapsable' : 'collapsable close'}>
+        {Array.isArray(props.content)
+          ? <ul>
+              {props.content.map((content, index) =>
+                <li key={index}>{content}</li>
+              )}
+            </ul>
+          : <p>{props.content}</p>
+        }
+      </div>
+    </div>
+  );
+}
+
+export default Collapse;
--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,8 +1,43 @@
 import './about.css';
+import Banner from '../../components/banner/Banner.js';
+import Collapse from '../../components/collapse/Collapse';
+import bannerAbout from '../../assets/img/banner-about.png';
 
 function About() {
   return (
-      <div>about</div>
+    <>
+      <section className='banner-about-container'>
+        <Banner img={bannerAbout} />
+      </section>
+      <section className='collapse-about-container'>
+        <Collapse
+          title="Fiabilité"
+          content="Les annonces postées sur Kasa garantissent une fiabilité totale. 
+            Les photos sont conformes aux logements, et toutes les informations sont 
+            régulièrement vérifiées  par nos équipes."
+        />
+        <Collapse
+          disabled={true}
+          title="Respect"
+          content="La bienveillance fait partie des valeurs fondatrices de Kasa. 
+            Tout comportement discriminatoire ou de perturbation du voisinage entraînera 
+            une exclusion de notre plateforme."
+        />
+        <Collapse
+          title="Service"
+          content="Nos équipes se tiennent à votre disposition pour vous fournir une 
+          expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+        />
+        <Collapse
+          title="Responsabilité"
+          content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour 
+          les voyageurs, chaque logement correspond aux critères de sécurité établis par 
+          nos services. En laissant une note aussi bien à l'hôte qu'au locataire, 
+          cela permet à nos équipes de vérifier que les standards sont bien respectés. 
+          Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+        />
+      </section>
+    </>
   );
 }
 
